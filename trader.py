6e8adf25c903bbdd37c4a17e1d3c3afa8b61e22c
Chris Fu <christopherfu2004@gmail.com>
2024-04-07 18:00:52 -0500

    shelter

--- trader.py
+++ trader.py
@@ -1,40 +1,45 @@
-from datamodel import OrderDepth, UserId, TradingState, Order
-from typing import List
-import string
+from typing import Any
+from datamodel import OrderDepth, TradingState, Order, Symbol, ProsperityEncoder
+import json
+from json import JSONEncoder
+import numpy as np
+import pandas as pd
+
 
 class Trader:
+    positions = {'AMETHYSTS': 0, 'STARFRUIT': 0}
+    position_limit = {'AMETHYSTS': 20, 'STARFRUIT': 20} 
     
     def run(self, state: TradingState):
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
-
-        # Orders to be placed on exchange matching engine
         result = {}
-        for product in state.order_depths:
-            order_depth: OrderDepth = state.order_depths[product]
-            orders: List[Order] = []
-            acceptable_price = 10  # Participant should calculate this value
-            print("Acceptable price : " + str(acceptable_price))
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
-            
-            result[product] = orders
-    
-        # String value holding Trader state data required. 
-        # It will be delivered as TradingState.traderData on next execution.
-        traderData = "SAMPLE" 
         
-        # Sample conversion request. Check more details below. 
+        for product in state.order_depths:
+            
+            if product == 'AMETHYSTS':  
+                order_depth: OrderDepth = state.order_depths[product]
+                orders: list[Order] = []
+                
+                acceptable_price = 10000
+                
+                if len(order_depth.sell_orders) != 0:
+                    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                    if int(best_ask) < acceptable_price and self.positions['AMETHYSTS'] < self.position_limit['AMETHYSTS']:
+                        buy_amount = min(self.position_limit['AMETHYSTS'] - self.positions['AMETHYSTS'],-best_ask_amount)
+                        print("BUY", str(-best_ask_amount) + "x", best_ask)
+                        orders.append(Order(product,best_ask,buy_amount))
+                
+                if len(order_depth.buy_orders) != 0:
+                    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                    if int(best_bid) > acceptable_price and self.positions['AMETHYSTS'] > -self.position_limit['AMETHYSTS']:
+                        sell_amount = min(self.positions['AMETHYSTS'] + self.position_limit['AMETHYSTS'], best_bid_amount)
+                        print("SELL", str(sell_amount) + "x", best_bid)
+                        orders.append(Order(product,best_bid,-sell_amount))
+                result[product] = orders
+                        
+                    
+
+                
+        traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        
         conversions = 1
         return result, conversions, traderData
