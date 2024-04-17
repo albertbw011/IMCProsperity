b3fc347cca955a91ab04007c0d62046925a2248f
Vivek Tallavajhula <vivek.tallav@utexas.edu>
2024-04-17 00:57:59 -0500

    stanford code, changed basket_std to 103 most profitable as of rn

--- trader3.py
+++ trader3.py
@@ -113,6 +113,14 @@ class Logger:
 
 logger = Logger()
 
+empty_dict = {'AMETHYSTS': 0, 'STARFRUIT': 0, 'ORCHIDS': 0, 'CHOCOLATE' : 0, 'STRAWBERRIES': 0, 'ROSES' : 0, 'GIFT_BASKET' : 0}
+
+
+def def_value():
+    return copy.deepcopy(empty_dict)
+
+INF = int(1e9)
+
 class Trader:
     positions = {'AMETHYSTS': 0, 'STARFRUIT': 0, 'ORCHIDS': 0, 'CHOCOLATE' : 0, 'STRAWBERRIES': 0, 'ROSES' : 0, 'GIFT_BASKET' : 0}
     position_limit = {'AMETHYSTS': 20, 'STARFRUIT': 20, 'ORCHIDS': 100, 'CHOCOLATE' : 250, 'STRAWBERRIES': 350, 'ROSES' : 60, 'GIFT_BASKET' : 60}
@@ -120,6 +128,49 @@ class Trader:
     price_log = {'STARFRUIT':[], 'ORCHIDS' : []}
     orchids_stats = {'SUNLIGHT' : [], 'HUMIDITY': [], 'EXPORT TARIFFS': [], 'IMPORT TARIFFS': [], 'SHIPPING COSTS': []}
     trending = {'SUNLIGHT' : 0, 'HUMIDITY' : 0}
+
+
+    position = copy.deepcopy(empty_dict)
+    POSITION_LIMIT = {'AMETHYSTS': 20, 'STARFRUIT': 20, 'ORCHIDS': 100, 'CHOCOLATE' : 250, 'STRAWBERRIES': 350, 'ROSES' : 60, 'GIFT_BASKET' : 60}
+    volume_traded = copy.deepcopy(empty_dict)
+
+    person_position = collections.defaultdict(def_value)
+    person_actvalof_position = collections.defaultdict(def_value)
+
+    cpnl = collections.defaultdict(lambda : 0)
+    bananas_cache = []
+    coconuts_cache = []
+    bananas_dim = 4
+    coconuts_dim = 3
+    steps = 0
+    last_dolphins = -1
+    buy_gear = False
+    sell_gear = False
+    buy_berries = False
+    sell_berries = False
+    close_berries = False
+    last_dg_price = 0
+    start_berries = 0
+    first_berries = 0
+    cont_buy_basket_unfill = 0
+    cont_sell_basket_unfill = 0
+    
+    halflife_diff = 5
+    alpha_diff = 1 - np.exp(-np.log(2)/halflife_diff)
+
+    halflife_price = 5
+    alpha_price = 1 - np.exp(-np.log(2)/halflife_price)
+
+    halflife_price_dip = 20
+    alpha_price_dip = 1 - np.exp(-np.log(2)/halflife_price_dip)
+    
+    begin_diff_dip = -INF
+    begin_diff_bag = -INF
+    begin_bag_price = -INF
+    begin_dip_price = -INF
+
+    std = 25
+    basket_std = 103
     
     def moving_average(self, item, period):
         '''
@@ -184,6 +235,79 @@ class Trader:
     #         self.trending = -1
     #     else:
     #         self.trending = 0
+    def compute_orders_basket2(self, order_depth):
+
+        orders = {'CHOCOLATE' : [], 'STRAWBERRIES': [], 'ROSES' : [], 'GIFT_BASKET' : []}
+        prods = ['CHOCOLATE', 'STRAWBERRIES', 'ROSES', 'GIFT_BASKET']
+        osell, obuy, best_sell, best_buy, worst_sell, worst_buy, mid_price, vol_buy, vol_sell = {}, {}, {}, {}, {}, {}, {}, {}, {}
+
+        for p in prods:
+            osell[p] = collections.OrderedDict(sorted(order_depth[p].sell_orders.items()))
+            obuy[p] = collections.OrderedDict(sorted(order_depth[p].buy_orders.items(), reverse=True))
+
+            best_sell[p] = next(iter(osell[p]))
+            best_buy[p] = next(iter(obuy[p]))
+
+            worst_sell[p] = next(reversed(osell[p]))
+            worst_buy[p] = next(reversed(obuy[p]))
+
+            mid_price[p] = (best_sell[p] + best_buy[p])/2
+            vol_buy[p], vol_sell[p] = 0, 0
+            for price, vol in obuy[p].items():
+                vol_buy[p] += vol 
+                if vol_buy[p] >= self.POSITION_LIMIT[p]/10:
+                    break
+            for price, vol in osell[p].items():
+                vol_sell[p] += -vol 
+                if vol_sell[p] >= self.POSITION_LIMIT[p]/10:
+                    break
+
+        res_buy = mid_price['GIFT_BASKET'] - mid_price['CHOCOLATE']*4 - mid_price['STRAWBERRIES']*6 - mid_price['ROSES'] - 375
+        res_sell = mid_price['GIFT_BASKET'] - mid_price['CHOCOLATE']*4 - mid_price['STRAWBERRIES']*6 - mid_price['ROSES'] - 375
+
+        trade_at = self.basket_std*0.5
+        close_at = self.basket_std*(-1000)
+
+        pb_pos = self.position['GIFT_BASKET']
+        pb_neg = self.position['GIFT_BASKET']
+
+        uku_pos = self.position['ROSES']
+        uku_neg = self.position['ROSES']
+
+
+        basket_buy_sig = 0
+        basket_sell_sig = 0
+
+        if self.position['GIFT_BASKET'] == self.POSITION_LIMIT['GIFT_BASKET']:
+            self.cont_buy_basket_unfill = 0
+        if self.position['GIFT_BASKET'] == -self.POSITION_LIMIT['GIFT_BASKET']:
+            self.cont_sell_basket_unfill = 0
+
+        do_bask = 0
+
+        if res_sell > trade_at:
+            vol = self.position['GIFT_BASKET'] + self.POSITION_LIMIT['GIFT_BASKET']
+            self.cont_buy_basket_unfill = 0 # no need to buy rn
+            assert(vol >= 0)
+            if vol > 0:
+                do_bask = 1
+                basket_sell_sig = 1
+                orders['GIFT_BASKET'].append(Order('GIFT_BASKET', worst_buy['GIFT_BASKET'], -vol)) 
+                self.cont_sell_basket_unfill += 2
+                pb_neg -= vol
+                #uku_pos += vol
+        elif res_buy < -trade_at:
+            vol = self.POSITION_LIMIT['GIFT_BASKET'] - self.position['GIFT_BASKET']
+            self.cont_sell_basket_unfill = 0 # no need to sell rn
+            assert(vol >= 0)
+            if vol > 0:
+                do_bask = 1
+                basket_buy_sig = 1
+                orders['GIFT_BASKET'].append(Order('GIFT_BASKET', worst_sell['GIFT_BASKET'], vol))
+                self.cont_buy_basket_unfill += 2
+                pb_pos += vol
+
+        return orders
 
     def determine_trend(self, value, datapts, period):
         """
@@ -501,7 +625,7 @@ class Trader:
 
                 # result[product] = orders
         
-        orders = self.compute_orders_basket(state.order_depths)
+        orders = self.compute_orders_basket2(state.order_depths)
         if len(orders['GIFT_BASKET']) != 0:
             print('bert3')
             result['GIFT_BASKET'] = orders['GIFT_BASKET']
